231a8337e998ec0910fecc8077dbfaf0a5878174
yhl452493373 <yhl452493373@gmail.com>
2025-11-21 23:25:39 +0800

    优化工作流，增加构建docker镜像的工作流

--- build-server.sh
+++ build-server.sh
@@ -16,12 +16,28 @@ echo "SPT服务端提交ID：$SPT_COMMIT_ID"
 echo "适用客户端版本：$CLIENT_VERSION"
 echo "构建日期：$DATE_TIME"
 
-docker buildx build \
-  --platform linux/amd64 \
-  -t yhl452493373/spt-server:$SPT_VERSION-$DATE_TIME-$SPT_COMMIT_ID \
+IMAGE_TAG="yhl452493373/spt-server:$SPT_VERSION-$DATE_TIME-$SPT_COMMIT_ID"
+
+echo "开始构建镜像"
+
+# 先删除可能存在的旧标签文件
+rm -rf image_tag.txt
+
+docker build \
+  -t $IMAGE_TAG \
   --build-arg SPT_VERSION=$SPT_VERSION \
   --build-arg SPT_BUILD_TYPE=$SPT_BUILD_TYPE \
   --build-arg SPT_BUILD_CONFIG=Release \
   -f Dockerfile .
 
+# 检查上一个命令的退出状态
+if [ $? -ne 0 ]; then
+  echo "错误：Docker 镜像构建失败！"
+  exit 1
+fi
+
 echo "镜像构建完毕"
+
+# 构建成功后，将镜像标签保存到文件
+echo "$IMAGE_TAG" > image_tag.txt
+echo "镜像标签已保存到 image_tag.txt: $IMAGE_TAG"
